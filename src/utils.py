75110d10bf98ccaca2574b4614771ba1583306ff
MeghaUkkali9 <meghaukkali99@gmail.com>
2026-02-04 14:01:17 +0530

    adding predict pipline

--- src/utils.py
+++ src/utils.py
@@ -1,9 +1,9 @@
 import os
 import sys
 import dill
-
 import numpy as np
 import pandas as pd
+
 from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
 from src.exception import CustomException
 
@@ -24,3 +24,10 @@ def evaluate_model(y_true, y_pred):
         rmse = np.sqrt(mse)
         r2 = r2_score(y_true, y_pred)
         return mse, mae, rmse, r2
+
+def load_object(file_path):
+    try:
+        with open(file_path, 'rb') as file_obj:
+            return dill.load(file_obj)
+    except Exception as e:
+        raise CustomException(e, sys)
